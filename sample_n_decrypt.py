fde0414d24e27901786a50bd667640d2449097e8
Jeffrey Lin <jeffrey82221@gmail.com>
2023-07-08 10:29:26 +0800

    Update sample_n_decrypt.py

--- sample_n_decrypt.py
+++ sample_n_decrypt.py
@@ -9,13 +9,19 @@ from src.json_tool import json_tool, JsonTool
 
 
 def run(src_path, target_path, sample_size):
-    files = os.listdir(src_path)
+    # files = os.listdir(src_path)
+    files = []
+    for dir_, _, fns in os.walk(src_path):
+        for fn in fns:
+            if fn.endswith('.json'):
+                files.append(os.path.join(dir_, fn))
     sampled_files = random.sample(files, sample_size)
     if not os.path.exists(target_path):
         os.mkdir(target_path)
     for file in tqdm.tqdm(sampled_files):
-        json_obj = json_tool.load(f"{src_path}/{file}")
-        JsonTool._dump_original(f"{target_path}/{file}", json_obj)
+        json_obj = json_tool.load(file)
+        json_file_name = file.split('/')[-1]
+        JsonTool._dump_original(f"{target_path}/{json_file_name}", json_obj)
 
 
 if __name__ == "__main__":
